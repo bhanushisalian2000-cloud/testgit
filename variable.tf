35230169347ec3bd46429cfcfc39ce76a5260477
bhanushisalian2000-cloud <bhanushisalian2000@gmail.com>
2026-09-01 13:55:12 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,49 @@
+variable "aws_region" {
+
+  description = "AWS region"
+
+  type        = string
+
+  default     = "ap-south-1"
+
+}
+ 
+variable "ami_id" {
+
+  description = "AMI ID for EC2"
+
+  type        = string
+
+}
+ 
+variable "instance_type" {
+
+  description = "EC2 instance type"
+
+  type        = string
+
+  default     = "t3.micro"
+
+}
+ 
+variable "instance_name" {
+
+  description = "EC2 instance name"
+
+  type        = string
+
+  default     = "terraform-ec2"
+
+}
+ 
+variable "environment" {
+
+  description = "Environment name"
+
+  type        = string
+
+  default     = "dev"
+
+}
+
+ 
