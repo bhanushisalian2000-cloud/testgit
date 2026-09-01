7702b8faa5df7dda147b3c3423017254ffd66c81
bhanushisalian2000-cloud <bhanushisalian2000@gmail.com>
2026-09-01 13:54:35 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+
+  required_version = ">= 1.5.0"
+ 
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "~> 6.0"
+
+    }
+
+  }
+
+}
+ 
+provider "aws" {
+
+  region = var.aws_region
+
+}
+ 
+resource "aws_instance" "ec2" {
+
+  ami           = var.ami_id
+
+  instance_type = var.instance_type
+ 
+  tags = {
+
+    Name        = var.instance_name
+
+    Environment = var.environment
+
+  }
+
+}
+
+ 
